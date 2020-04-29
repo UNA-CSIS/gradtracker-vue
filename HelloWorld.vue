6e0a4174846a85fbb6d20cdc9355a8f1a286b888
WBrasher <60417570+WBrasher@users.noreply.github.com>
2020-04-29 10:50:17 -0500

    Update HelloWorld.vue

--- HelloWorld.vue
+++ HelloWorld.vue
@@ -1,62 +1,4 @@
 <template>
-
-  <v-container>
-    <!-- provides the ability to center and horizontally pad your site's contents. -->
-    <v-row class="text-center">
-      <!--Centers text on screen -->
-      <v-col cols="12">
-        <!-- Below is the code used to add UNA image into Home page -->
-        <!--   <v-img          
-                :src="require('../assets/UNALogo.png')"
-                class="my-4"
-                contain      
-                height="200"
-        />-->
-      </v-col>
-
-      <template>
-        <v-row>
-          <v-col cols="12">
-            <v-card>
-              <v-col class="mb-4">
-                <h1 class="display-2 font-weight-bold mb-3">
-                  <!--makes the text below bold -->
-                  Welcome to the GradTracker
-                </h1>
-                <v-img :src="require('../assets/UNALogo.png')" class="my-4" contain height="200" />
-              </v-col>
-              <v-card-title>
-                Student Search
-                <v-spacer></v-spacer>
-                <v-text-field
-                  v-model="search"
-                  append-icon="mdi-magnify"
-                  label="Search"
-                  single-line
-                  hide-details
-                ></v-text-field>
-              </v-card-title>
-              <v-data-table
-                :headers="headers"
-                :items="students"
-                :items-per-page="5"
-                :search="search"
-                class="elevation-1"
-              >
-                <template v-slot:item.actions="{ item }">
-                  <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
-                  <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
-                </template>
-              </v-data-table>
-            </v-card>
-          </v-col>
-        </v-row>
-      </template>
-    </v-row>
-  </v-container>
-</template>
-
-
   <v-data-table
     :headers="headers"
     :items="students"
@@ -65,30 +7,23 @@
     class="elevation-1"
   >
     <template v-slot:top>
-      
-      <h1 class="display-2 font-weight-bold mb-3 text-center">      <!--makes the text below bold -->
-        Welcome to the GradTracker 
+      <h1 class="display-2 font-weight-bold mb-3 text-center">
+        <!--makes the text below bold -->
+        Welcome to the GradTracker
       </h1>
-      <v-img          
-        :src="require('../assets/UNALogo.png')"
-        class="my-4"
-        contain      
-        height="200"
-      /> 
-      
+      <v-img :src="require('../assets/UNALogo.png')" class="my-4" contain height="200" />
+
       <v-card-title>
-          <v-text-field
-            v-model="search"
-            append-icon="mdi-magnify"
-            label="Student Search"
-            single-line
-            hide-details
-            text-center
-          ></v-text-field>
-        </v-card-title>
-            
-          
-          
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Student Search"
+          single-line
+          hide-details
+          text-center
+        ></v-text-field>
+      </v-card-title>
+
       <v-toolbar flat color="white">
         <v-dialog v-model="dialog" max-width="600px">
           <v-card>
@@ -96,27 +31,26 @@
               <span class="headline">{{ formTitle }}</span>
             </v-card-title>
             <v-card-text>
-                <v-row>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.name" label="Name"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.gradYear" label="Graduation Year"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.phone" label="Phone Number"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.email" label="Email"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.major" label="Major"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.employer" label="Employer"></v-text-field>
-                  </v-col>
-                </v-row>
-              
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.name" label="Name"></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.gradYear" label="Graduation Year"></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.phone" label="Phone Number"></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.email" label="Email"></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.major" label="Major"></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.employer" label="Employer"></v-text-field>
+                </v-col>
+              </v-row>
             </v-card-text>
 
             <v-card-actions>
@@ -129,19 +63,8 @@
       </v-toolbar>
     </template>
     <template v-slot:item.actions="{ item }">
-      <v-icon
-        small
-        class="mr-2"
-        @click="editItem(item)"
-      >
-        mdi-pencil
-      </v-icon>
-      <v-icon
-        small
-        @click="deleteItem(item)"
-      >
-        mdi-delete
-      </v-icon>
+      <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+      <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
     </template>
     <template v-slot:no-data>
       <v-btn color="primary" @click="initialize">Reset</v-btn>
@@ -149,11 +72,14 @@
   </v-data-table>
 </template>
 
+
+
+
 <script>
 export default {
   data() {
     return {
-
+      search: "",
       headers: [
         {
           text: "Name",
@@ -188,19 +114,19 @@ export default {
       }
     };
   },
-
   computed: {
     formTitle() {
       return this.editedIndex === -1 ? "New Item" : "Edit Item";
     }
   },
-
   watch: {
     dialog(val) {
       val || this.close();
     }
   },
-
+  created() {
+    this.initialize();
+  },
   methods: {
     initialize() {
       this.students = [
@@ -275,13 +201,11 @@ export default {
       this.editedItem = Object.assign({}, item);
       this.dialog = true;
     },
-
     deleteItem(item) {
       const index = this.students.indexOf(item);
       confirm("Are you sure you want to delete this item?") &&
         this.students.splice(index, 1);
     },
-
     close() {
       this.dialog = false;
       setTimeout(() => {
@@ -289,7 +213,6 @@ export default {
         this.editedIndex = -1;
       }, 300);
     },
-
     save() {
       if (this.editedIndex > -1) {
         Object.assign(this.students[this.editedIndex], this.editedItem);
@@ -300,3 +223,4 @@ export default {
     }
   }
 };
+</script>
