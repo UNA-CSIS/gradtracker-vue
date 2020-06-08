10e332b3aacf0aee94856fbf83a8affcfbe10039
WBrasher <60417570+WBrasher@users.noreply.github.com>
2020-06-08 15:02:37 -0500

    Add files via upload

--- HelloWorld.vue
+++ HelloWorld.vue
@@ -1,90 +1,121 @@
 
 <template>
-  <v-app>
-    <v-container fluid>
-      <v-row>
-        <v-col cols="12">
-          <v-card
-            class="pa-md-4 mb-4 mx-auto"
-            elevation="10"
-            color="grey lighten-3"
-            width="50%"
-            v-for="student in students"
-            :key="student.id"
-          >
-            <v-card-title
-              class="headline primary--text"
-              primary-title
-            >{{ student.firstName + " " + student.lastName }}</v-card-title>
+  <v-data-table
+    :headers="headers"
+    :items="students"
+    :search="search"
+    :items-per-page="5"
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <h1 class="display-2 font-weight-bold mb-3 text-center">
+        <!--makes the text below bold -->
+        Welcome to the GradTracker
+      </h1>
+      <v-img :src="require('../assets/UNALogo.png')" class="my-4" contain height="200" />
 
-            <v-row>
-              <v-col xs12 md6>
-                <div class="caption grey--text text--darken-2">Student ID</div>
-                <div>{{ student.studentID }}</div>
-              </v-col>
+      <v-card-title>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Student Search"
+          single-line
+          hide-details
+          text-center
+        ></v-text-field>
+      </v-card-title>
 
-              <v-col xs12 md6>
-                <div class="caption grey--text text--darken-2">Major</div>
-                <div>{{ student.major }}</div>
-              </v-col>
-              <v-col xs12 md6>
-                <div class="caption grey--text text--darken-2">Email</div>
-                <div>{{ student.email }}</div>
-              </v-col>
-              <v-col xs12 md6>
-                <div class="caption grey--text text--darken-2">Phone</div>
-                <div>{{ student.phone }}</div>
-              </v-col>
-              <v-col xs12 md6>
-                <div class="caption grey--text text--darken-2">Employer</div>
-                <div>{{ student.employer }}</div>
-              </v-col>
-              <v-col xs12 md6>
-                <div class="caption grey--text text--darken-2">Birthday</div>
-                <div>{{ student.birthday }}</div>
-              </v-col>
-              <v-col xs12 md6>
-                <div class="caption grey--text text--darken-2">Employed</div>
-                <div>{{ student.employed }}</div>
-              </v-col>
+      <v-toolbar flat color="white">
+        <v-dialog v-model="dialog" max-width="600px">
+          <v-card>
+            <v-card-title>
+              <span class="headline">{{ formTitle }}</span>
+            </v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.name" label="Name"></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.gradYear" label="Graduation Year"></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.phone" label="Phone Number"></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.email" label="Email"></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.major" label="Major"></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.employer" label="Employer"></v-text-field>
+                </v-col>
+              </v-row>
+            </v-card-text>
 
-              <v-col xs12 md6>
-                <div class="caption grey--text text--darken-2">Semester</div>
-                <div>{{ student.semesters }}</div>
-              </v-col>
-              <v-col xs12 md6>
-                <div class="caption grey--text text--darken-2">Position</div>
-                <div>{{ student.position }}</div>
-              </v-col>
-
-              <v-col xs12 md6>
-                <div class="caption grey--text text--darken-2">Grad Year</div>
-                <div>{{ student.graduation }}</div>
-              </v-col>
-              <v-col xs12 md6>
-                <div class="caption grey--text text--darken-2">Resume</div>
-              </v-col>
-            </v-row>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="purple" text @click="close">Cancel</v-btn>
+              <v-btn color="purple" text @click="save">Save</v-btn>
+            </v-card-actions>
           </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-app>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+      <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn color="primary" @click="initialize">Reset</v-btn>
+    </template>
+  </v-data-table>
 </template>
-
 <script>
 import db from "@/fb2";
-//import firebase from "firebase";
 export default {
   data() {
     return {
-      students: []
+      search: "",
+      headers: [
+        {
+          text: "First Name",
+          align: "start",
+          sortable: true,
+          value: "firstName"
+        },
+        { text: "Last Name", value: "lastName", sortable: true },
+        { text: "Student ID", value: "studentID", sortable: true },
+        { text: "Graduation Year", value: "semesters", sortable: true },
+        { text: "Phone Number", value: "phone", sortable: false },
+        { text: "Birthday", value: "birthday", sortable: true },
+        { text: "Email", value: "email", sortable: false },
+        { text: "Major", value: "major", sortable: true },
+        { text: "Employed", value: "employed", sortable: true },
+        { text: "Employer", value: "employer", sortable: true },
+        { text: "Position", value: "position", sortable: true },
+        { text: "Edit/Delete", value: "actions", sortable: false }
+      ],
+      students: [],
+      editedIndex: -1,
+      editedItem: {
+        firstName: "",
+        gradYear: -1,
+        phone: "",
+        email: "",
+        major: "",
+        employer: ""
+      },
+      defaultItem: {
+        name: "",
+        gradYear: 0,
+        phone: "",
+        email: "",
+        major: "",
+        employer: ""
+      }
     };
-  },
-  methods: {
-    sortBy(prop) {
-      this.students.sort((a, b) => (a[prop] < b[prop] ? -1 : 1));
-    }
   },
   // }
   // Pull updated data from database
